5a036ecde874d4935ba1fc3e76e019c8571d72c2
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-02-20 12:10:56 +0000

    add basic function declaration examples

--- hello.js
+++ hello.js
@@ -1,1 +1,13 @@
-console.log('Hello - I am a file!');
+console.log("Hello - I am a file!");
+console.log("Hello world");
+console.log(2 + 2);
+let MyName = "Edward";
+
+console.log(`Hello, ${MyName}`);
+
+// fix this file
+
+const greeting =('Hello ');
+const name =('Makers');
+
+console.log(greeting + name);
