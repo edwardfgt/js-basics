42245617376b173f9c528168957dabbaee38ca0d
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-02-21 11:33:30 +0000

    create second test

--- testing-with-jest/add.test.js
+++ testing-with-jest/add.test.js
@@ -1,8 +1,20 @@
 // file: add.test.js
-const add = require('./add')
+const add = require('./add');
+const multiply = require('./multiply');
 
 describe('add', () => {
   it('Adds 2 and 2 together', () => {
     expect(add(2, 2)).toBe(4);
+  });
+
+  it('Adds 5 and 0 together', () => {
+    expect(add(5, 0)).toBe(5);
+  });
+});
+
+
+describe('multiply', () => {
+  it('multiplies 5 and 2 together', () => {
+    expect(multiply(5, 2)).toBe(10);
   });
 });
