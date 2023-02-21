3cd087fe6b6931e4842e692bb19447c4eada9551
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-02-21 10:42:22 +0000

    play around with complementary classes

--- classes.js
+++ classes.js
@@ -36,18 +36,34 @@ class User {
   }
 }
 
-class userBase {
-  constructor(user) {
-    this.user = user;
+class UserBase {
+  constructor(users) {
+    this.users = users;
+  }
+
+  count(){
+    return this.users.length;
   }
 
   getNames(){
-
+    return this.users;
   }
 
   getIntroductions(){
-    
+    return this.users.map(user => user.getIntroduction())
   }
 }
 
-module.exports = User;
+
+
+const users = [
+  new User('Uma'),
+  new User('Josh'),
+  new User('Ollie')
+];
+
+const userBase = new UserBase(users);
+console.log(userBase.count());
+console.log(userBase.getNames());
+console.log(userBase.getIntroductions());
+
